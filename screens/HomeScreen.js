d514cee64340e850ebb9009d296791a0a950c625
doanhuyxh <doanhuyxh@gmail.com>
2023-09-06 18:45:49 +0700

    xong, thiếu custom thông báo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useLayoutEffect, useState} from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {ScrollView, View} from 'react-native';
+import {ScrollView, View, TextInput, TouchableOpacity} from 'react-native';
 import {useDispatch, useSelector} from "react-redux";
+import {AntDesign} from '@expo/vector-icons';
 import ItemCategory from "../components/ItemCategory";
 import axios from "../Config/Axios";
 import {getSelectedCategory} from "../slices/SelectedCategorySlice";
@@ -13,7 +14,19 @@ function HomeScreen() {
     const [listCate, setListCate] = useState([]);
     const [listNews, setListNews] = useState([]);
     const [cur, setCur] = useState([]);
+    const [inputText, setInputText] = useState("");
 
+    const HandleSreach = ()=>{
+        let data;
+        if(categorySelected == 0){
+            data = listNews.filter(i=>i.title.includes(inputText))
+        }
+        else{
+        data= listNews.filter(i=>i.cateID.includes(categorySelected) && i.title.includes(inputText))
+        }
+        console.log("data search", data.length)
+        setCur(data)
+    }
     useEffect(() => {
         axios.get("/api/v1/Items/GetAllCategoryMobile")
             .then(data => {
@@ -33,28 +46,40 @@ function HomeScreen() {
                     list.push(data.products[i])
                 }
                 setListNews(list);
+                setCur(list);
             })
     }, []);
 
-    useLayoutEffect(()=>{
-        if(categorySelected == 0) {
+    useLayoutEffect(() => {
+        if (categorySelected == 0) {
             setCur(listNews);
-        }
-        else{
-            let newss = listNews.filter(i=>i.cateID.includes(categorySelected));
+        } else {
+            let newss = listNews.filter(i => i.cateID.includes(categorySelected));
             setCur(newss);
         }
-    },[categorySelected])
+    }, [categorySelected])
     return (
-        <SafeAreaView>
+        <SafeAreaView className="pt-0.5">
             <ScrollView>
-                <View className="flex-row px-2 h-12 bg-amber-300 align-middle">
+                <View className="w-full bg-red-700 h-16 justify-center align-middle relative inline-block">
+                    <TextInput
+                        className="h-12 bg-white px-2 mx-3 rounded-2xl"
+                        placeholder="Nhập từ khóa"
+                        value={inputText}
+                        onChangeText={(text) => setInputText(text)}
+                    />
+                    <TouchableOpacity className="absolute bottom-4 right-6" onPress={HandleSreach}>
+                        <AntDesign name="search1" size={32} color="black"/>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-row px-2 h-12 align-middle">
                     {
                         listCate.map((item, index) => {
                             return <ItemCategory data={item} key={index}/>
                         })
                     }
                 </View>
+                <View className="w-full h-0.5 bg-yellow-50"></View>
                 <View className="flex-col">
                     {
                         cur.map((item, index) => {
